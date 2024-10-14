f57667f150f1f49baab95a178fb9da606b0e5c30
Maxim Khan-Magomedov <maxim.km@gmail.com>
2024-10-14 11:37:09 +0300

    Use data from JSON file

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,13 @@
 const express = require('express');
+const path = require('path')
+const fs = require('fs')
 const router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Stereo.photo' });
+  const data = JSON.parse(fs.readFileSync(path.join(__dirname, '../lib/photos.json'), 'utf8'))
+
+  res.render('index', { title: 'Stereo.photo', photo: data[0] });
 });
 
 module.exports = router;
