950c5ecc8df45e50041bc1932c71bb0b50218483
Maxim Khan-Magomedov <maxim.km@gmail.com>
2024-10-16 16:05:29 +0300

    Use page numbers

--- routes/photos.js
+++ routes/photos.js
@@ -5,8 +5,11 @@ const router = express.Router();
 /* GET home page. */
 router.get('/', (req, res, next) => {
   const handler = new PhotoHandler()
+  const pageFromQuery = parseInt(req.query.page)
+  const parsedPage = !pageFromQuery ? 1 : pageFromQuery
+  const page = parsedPage < 1 ? 1 : parsedPage
 
-  res.render('photos', { title: 'All stereo photos', photos: handler.page(1) });
+  res.render('photos', { title: 'All stereo photos', photos: handler.page(page), page: page });
 });
 
 module.exports = router;
