156dd0bf81a91f0b8f40e0b222af9d6f0adfe07f
Maxim Khan-Magomedov <maxim.km@gmail.com>
2024-10-16 16:22:06 +0300

    Add pagination without styling

--- routes/photos.js
+++ routes/photos.js
@@ -5,11 +5,9 @@ const router = express.Router();
 /* GET home page. */
 router.get('/', (req, res, next) => {
   const handler = new PhotoHandler()
-  const pageFromQuery = parseInt(req.query.page)
-  const parsedPage = !pageFromQuery ? 1 : pageFromQuery
-  const page = parsedPage < 1 ? 1 : parsedPage
+  const pagination = handler.pagination(parseInt(req.query.page))
 
-  res.render('photos', { title: 'All stereo photos', photos: handler.page(page), page: page });
+  res.render('photos', { title: 'All stereo photos', photos: handler.page(pagination.current), pagination: pagination });
 });
 
 module.exports = router;
