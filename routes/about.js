fdf51c50d5d9ef6abccb34ac3ef3a23eb7ce13a4
Maxim Khan-Magomedov <maxim.km@gmail.com>
2024-10-17 01:17:48 +0300

    Create single photo page

--- routes/about.js
+++ routes/about.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 
-/* GET users listing. */
+/* GET about page. */
 router.get('/', (req, res, next) => {
   res.render('about', { title: 'About stereo photos' });
 });
--- routes/photos.js
+++ routes/photos.js
@@ -2,12 +2,26 @@ const express = require('express');
 const PhotoHandler = require('../lib/photo_handler')
 const router = express.Router();
 
-/* GET home page. */
+/* GET list of photos. */
 router.get('/', (req, res, next) => {
   const handler = new PhotoHandler()
   const pagination = handler.pagination(parseInt(req.query.page))
 
-  res.render('photos', { title: 'All stereo photos', photos: handler.page(pagination.current), pagination: pagination });
+  res.render('photos', { title: 'All stereo photos', photos: handler.page(pagination.current), pagination });
+});
+
+/* GET photo page. */
+router.get('/:slug', (req, res, next) => {
+  const handler = new PhotoHandler()
+  const photo = handler.find(req.params.slug)
+
+  if (!photo) {
+    const err = new Error('Cannot find photo')
+    err.status = 404
+    throw err
+  }
+
+  res.render('photo', { photo });
 });
 
 module.exports = router;
